a69c4232492679bb33e03d98508d01a7f5f37abe
Paulo Rivera <josepauloovallerivera@gmail.com>
2020-10-03 17:22:34 +0900

    Tests
    - Updated .gitignore

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
+  console.log('hey')
   res.send('Hey! It works!');
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('') 
+  res.send(reverse);
+});
+
 
 module.exports = router;
